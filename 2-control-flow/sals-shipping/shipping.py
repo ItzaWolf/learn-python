a36e3bad6769741ed7fd04b17958173e6732ea87
ItzaWolf <141709348+ItzaWolf@users.noreply.github.com>
2023-08-08 00:29:40 -0600

    Update shipping.py
    This optimizes the process by telling you at the end what is the lowest cost option! Second day learning python and programing in general! (So let me know if there is an easier way to accomplish this!)

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,46 @@
-# Sal's Shipping
-# Sonny Li
+# Shipping options with additional challenge at the end
+# Kaleb Bolack
 
-weight = 80
+weight = 100
+cost = ''
 
-# Ground Shipping 🚚
-
+# Ground Shipping Here
+ 
 if weight <= 2:
   cost_ground = weight * 1.5 + 20
 elif weight <= 6:
-  cost_ground = weight * 3.00 + 20
+  cost_ground = weight * 3 + 20
 elif weight <= 10:
-  cost_ground = weight * 4.00 + 20
+  cost_ground = weight * 4 + 20
 else:
   cost_ground = weight * 4.75 + 20
+print('Ground Shipping (Based on Weight in Ibs | includes Flat Fee) $',cost_ground)
 
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
+# Ground Shipping PREMIUM Here
 
-cost_ground_premium = 125.00
+cost_ground_premium = 125
+print('Ground Shipping Premium (Flat Rate): $', cost_ground_premium)
 
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
+# Drone Shipping Here
 
 if weight <= 2:
-  cost_drone = weight * 4.5
+  cost_drone = weight * 4.50
 elif weight <= 6:
-  cost_drone = weight * 9.00
+  cost_drone = weight * 9
 elif weight <= 10:
-  cost_drone = weight * 12.00
-else:
+  cost_drone = weight * 12
+elif weight >= 11:
   cost_drone = weight * 14.25
+print('Drone Shipping (Based on Weight in Ibs) $', cost_drone)
 
-print("Drone Shipping: $", cost_drone)
+# Challenge: Try and get the code to identify the best option for the customer!
+
+low_cost_option = min(cost_ground, cost_ground_premium, cost_drone)
+
+if low_cost_option == cost_ground:
+  method = "Ground Shipping"
+elif low_cost_option == cost_ground_premium:
+  method = "Ground Shipping Premium"
+elif low_cost_option == cost_drone:
+  method = "Drone Shipping"
+print("The lowest cost shipment package will be", method)
